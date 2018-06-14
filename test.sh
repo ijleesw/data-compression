13ea2b7b707fb5c4b36b2b43fa0b9109c46b8b2c
ijleesw <ijleesw@gmail.com>
2018-06-14 19:14:53 +0900

    Edit test.sh

--- test.sh
+++ test.sh
@@ -16,36 +16,6 @@ make clean
 make
 
 
-echo "Running Tunstall Code Compression/Decompression"
-./tunstall data/SD1 result/tunstall/SD1_compressed result/tunstall/SD1_decompressed
-./tunstall data/SD2 result/tunstall/SD2_compressed result/tunstall/SD2_decompressed
-./tunstall data/SD3 result/tunstall/SD3_compressed result/tunstall/SD3_decompressed
-./tunstall data/SD4 result/tunstall/SD4_compressed result/tunstall/SD4_decompressed
-./tunstall data/dnaby result/tunstall/dnaby_compressed result/tunstall/dnaby_decompressed
-./tunstall data/englishby result/tunstall/englishby_compressed result/tunstall/englishby_decompressed
-./tunstall data/xmlby result/tunstall/xmlby_compressed result/tunstall/xmlby_decompressed
-
-
-echo "Running Golomb Code Compression/Decompression"
-./golomb data/SD1 result/golomb/SD1_compressed result/golomb/SD1_decompressed
-./golomb data/SD2 result/golomb/SD2_compressed result/golomb/SD2_decompressed
-./golomb data/SD3 result/golomb/SD3_compressed result/golomb/SD3_decompressed
-./golomb data/SD4 result/golomb/SD4_compressed result/golomb/SD4_decompressed
-./golomb data/dnaby result/golomb/dnaby_compressed result/golomb/dnaby_decompressed
-./golomb data/englishby result/golomb/englishby_compressed result/golomb/englishby_decompressed
-./golomb data/xmlby result/golomb/xmlby_compressed result/golomb/xmlby_decompressed
-
-
-echo "Running Arithmetic Coding Compression/Decompression"
-./arithmetic data/SD1 result/arithmetic/SD1_compressed result/arithmetic/SD1_decompressed
-./arithmetic data/SD2 result/arithmetic/SD2_compressed result/arithmetic/SD2_decompressed
-./arithmetic data/SD3 result/arithmetic/SD3_compressed result/arithmetic/SD3_decompressed
-./arithmetic data/SD4 result/arithmetic/SD4_compressed result/arithmetic/SD4_decompressed
-./arithmetic data/dnaby result/arithmetic/dnaby_compressed result/arithmetic/dnaby_decompressed
-./arithmetic data/englishby result/arithmetic/englishby_compressed result/arithmetic/englishby_decompressed
-./arithmetic data/xmlby result/arithmetic/xmlby_compressed result/arithmetic/xmlby_decompressed
-
-
 echo "Running Static Huffman Code Compression/Decompression"
 python src/main.py -c static_huffman -i data/SD1 -v
 python src/main.py -c static_huffman -i data/SD2 -v
@@ -66,6 +36,36 @@ python src/main.py -c adaptive_huffman -i data/englishby -v
 python src/main.py -c adaptive_huffman -i data/xmlby -v
 
 
+echo "Running Golomb Code Compression/Decompression"
+./golomb data/SD1 result/golomb/SD1_compressed result/golomb/SD1_decompressed
+./golomb data/SD2 result/golomb/SD2_compressed result/golomb/SD2_decompressed
+./golomb data/SD3 result/golomb/SD3_compressed result/golomb/SD3_decompressed
+./golomb data/SD4 result/golomb/SD4_compressed result/golomb/SD4_decompressed
+./golomb data/dnaby result/golomb/dnaby_compressed result/golomb/dnaby_decompressed
+./golomb data/englishby result/golomb/englishby_compressed result/golomb/englishby_decompressed
+./golomb data/xmlby result/golomb/xmlby_compressed result/golomb/xmlby_decompressed
+
+
+echo "Running Tunstall Code Compression/Decompression"
+./tunstall data/SD1 result/tunstall/SD1_compressed result/tunstall/SD1_decompressed
+./tunstall data/SD2 result/tunstall/SD2_compressed result/tunstall/SD2_decompressed
+./tunstall data/SD3 result/tunstall/SD3_compressed result/tunstall/SD3_decompressed
+./tunstall data/SD4 result/tunstall/SD4_compressed result/tunstall/SD4_decompressed
+./tunstall data/dnaby result/tunstall/dnaby_compressed result/tunstall/dnaby_decompressed
+./tunstall data/englishby result/tunstall/englishby_compressed result/tunstall/englishby_decompressed
+./tunstall data/xmlby result/tunstall/xmlby_compressed result/tunstall/xmlby_decompressed
+
+
+echo "Running Arithmetic Coding Compression/Decompression"
+./arithmetic data/SD1 result/arithmetic/SD1_compressed result/arithmetic/SD1_decompressed
+./arithmetic data/SD2 result/arithmetic/SD2_compressed result/arithmetic/SD2_decompressed
+./arithmetic data/SD3 result/arithmetic/SD3_compressed result/arithmetic/SD3_decompressed
+./arithmetic data/SD4 result/arithmetic/SD4_compressed result/arithmetic/SD4_decompressed
+./arithmetic data/dnaby result/arithmetic/dnaby_compressed result/arithmetic/dnaby_decompressed
+./arithmetic data/englishby result/arithmetic/englishby_compressed result/arithmetic/englishby_decompressed
+./arithmetic data/xmlby result/arithmetic/xmlby_compressed result/arithmetic/xmlby_decompressed
+
+
 echo "Running LZ77 Compression/Decompression"
 python src/main.py -c lz77 -i data/SD1 -v
 python src/main.py -c lz77 -i data/SD2 -v
@@ -84,7 +84,6 @@ echo "Running LZ78 Compression/Decompression"
 ./lz78 -i data/dnaby -o result/lz78/dnaby_compressed
 ./lz78 -i data/englishby -o result/lz78/englishby_compressed
 ./lz78 -i data/xmlby -o result/lz78/xmlby_compressed
-rm -r lz78.dSYM
 ./lz78 -i result/lz78/SD1_compressed -o result/lz78/SD1_decompressed -d
 ./lz78 -i result/lz78/SD2_compressed -o result/lz78/SD2_decompressed -d
 ./lz78 -i result/lz78/SD3_compressed -o result/lz78/SD3_decompressed -d
@@ -172,38 +171,6 @@ echo ""
 # How to use checker:
 # Add ./checker <data name> <original file path> <compressed file path> <decompressed file path>
 
-echo "Compression Ratio of Tunstall Code"
-./checker SD1 data/SD1 result/tunstall/SD1_compressed result/tunstall/SD1_decompressed
-./checker SD2 data/SD2 result/tunstall/SD2_compressed result/tunstall/SD2_decompressed
-./checker SD3 data/SD3 result/tunstall/SD3_compressed result/tunstall/SD3_decompressed
-./checker SD4 data/SD4 result/tunstall/SD4_compressed result/tunstall/SD4_decompressed
-./checker dnaby data/dnaby result/tunstall/dnaby_compressed result/tunstall/dnaby_decompressed
-./checker englishby data/englishby result/tunstall/englishby_compressed result/tunstall/englishby_decompressed
-./checker xmlby data/xmlby result/tunstall/xmlby_compressed result/tunstall/xmlby_decompressed
-echo ""
-
-
-echo "Compression Ratio of Golomb Code"
-./checker SD1 data/SD1 result/golomb/SD1_compressed result/golomb/SD1_decompressed
-./checker SD2 data/SD2 result/golomb/SD2_compressed result/golomb/SD2_decompressed
-./checker SD3 data/SD3 result/golomb/SD3_compressed result/golomb/SD3_decompressed
-./checker SD4 data/SD4 result/golomb/SD4_compressed result/golomb/SD4_decompressed
-./checker dnaby data/dnaby result/golomb/dnaby_compressed result/golomb/dnaby_decompressed
-./checker englishby data/englishby result/golomb/englishby_compressed result/golomb/englishby_decompressed
-./checker xmlby data/xmlby result/golomb/xmlby_compressed result/golomb/xmlby_decompressed
-echo ""
-
-
-echo "Compression Ratio of Arithmetic Coding"
-./checker SD1 data/SD1 result/arithmetic/SD1_compressed result/arithmetic/SD1_decompressed
-./checker SD2 data/SD2 result/arithmetic/SD2_compressed result/arithmetic/SD2_decompressed
-./checker SD3 data/SD3 result/arithmetic/SD3_compressed result/arithmetic/SD3_decompressed
-./checker SD4 data/SD4 result/arithmetic/SD4_compressed result/arithmetic/SD4_decompressed
-./checker dnaby data/dnaby result/arithmetic/dnaby_compressed result/arithmetic/dnaby_decompressed
-./checker englishby data/englishby result/arithmetic/englishby_compressed result/arithmetic/englishby_decompressed
-./checker xmlby data/xmlby result/arithmetic/xmlby_compressed result/arithmetic/xmlby_decompressed
-echo ""
-
 
 echo "Compression Ratio of Static Huffman Code"
 ./checker SD1 data/SD1 result/static_huffman/SD1_compressed result/static_huffman/SD1_decompressed
@@ -224,6 +191,39 @@ echo "Compression Ratio of Adaptive Huffman Code"
 ./checker dnaby data/dnaby result/adaptive_huffman/dnaby_compressed result/adaptive_huffman/dnaby_decompressed
 ./checker englishby data/englishby result/adaptive_huffman/englishby_compressed result/adaptive_huffman/englishby_decompressed
 ./checker xmlby data/xmlby result/adaptive_huffman/xmlby_compressed result/adaptive_huffman/xmlby_decompressed
+echo ""
+
+
+echo "Compression Ratio of Golomb Code"
+./checker SD1 data/SD1 result/golomb/SD1_compressed result/golomb/SD1_decompressed
+./checker SD2 data/SD2 result/golomb/SD2_compressed result/golomb/SD2_decompressed
+./checker SD3 data/SD3 result/golomb/SD3_compressed result/golomb/SD3_decompressed
+./checker SD4 data/SD4 result/golomb/SD4_compressed result/golomb/SD4_decompressed
+./checker dnaby data/dnaby result/golomb/dnaby_compressed result/golomb/dnaby_decompressed
+./checker englishby data/englishby result/golomb/englishby_compressed result/golomb/englishby_decompressed
+./checker xmlby data/xmlby result/golomb/xmlby_compressed result/golomb/xmlby_decompressed
+echo ""
+
+
+echo "Compression Ratio of Tunstall Code"
+./checker SD1 data/SD1 result/tunstall/SD1_compressed result/tunstall/SD1_decompressed
+./checker SD2 data/SD2 result/tunstall/SD2_compressed result/tunstall/SD2_decompressed
+./checker SD3 data/SD3 result/tunstall/SD3_compressed result/tunstall/SD3_decompressed
+./checker SD4 data/SD4 result/tunstall/SD4_compressed result/tunstall/SD4_decompressed
+./checker dnaby data/dnaby result/tunstall/dnaby_compressed result/tunstall/dnaby_decompressed
+./checker englishby data/englishby result/tunstall/englishby_compressed result/tunstall/englishby_decompressed
+./checker xmlby data/xmlby result/tunstall/xmlby_compressed result/tunstall/xmlby_decompressed
+echo ""
+
+
+echo "Compression Ratio of Arithmetic Coding"
+./checker SD1 data/SD1 result/arithmetic/SD1_compressed result/arithmetic/SD1_decompressed
+./checker SD2 data/SD2 result/arithmetic/SD2_compressed result/arithmetic/SD2_decompressed
+./checker SD3 data/SD3 result/arithmetic/SD3_compressed result/arithmetic/SD3_decompressed
+./checker SD4 data/SD4 result/arithmetic/SD4_compressed result/arithmetic/SD4_decompressed
+./checker dnaby data/dnaby result/arithmetic/dnaby_compressed result/arithmetic/dnaby_decompressed
+./checker englishby data/englishby result/arithmetic/englishby_compressed result/arithmetic/englishby_decompressed
+./checker xmlby data/xmlby result/arithmetic/xmlby_compressed result/arithmetic/xmlby_decompressed
 echo ""
 
 
